adb48c3fcad6e4525010a85293b8e101c8f3fd00
Soofiaaa <rootroot1234@outlook.fr>
2021-11-08 22:48:16 -0500

    New .gitingore

--- extensions/__init__.py
+++
@@ -1,9 +0,0 @@
-from client.client import Client
-import sys, pymem
-
-"""The client can be accessed by every extensions like this."""
-try:
-    client = Client()
-except pymem.exception.ProcessNotFound as e:
-    print(e)
-    sys.exit()
