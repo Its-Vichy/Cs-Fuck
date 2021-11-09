f03a11c40e9bf217483b85a339a4f9a5390cc7a2
Soofiaaa <rootroot1234@outlook.fr>
2021-11-08 22:44:58 -0500

    New modules and new version

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@ from setuptools import *
 setup(
     name='Cs-Fuck',
     description='Cs-Fuck MultiHack',
-    version='0.0.2',
-    author='Sofia, Its-Vichy',
+    version='0.0.4',
+    author='Its-Vichy, Sofia',
     python_require='>=3.0',
     packages=find_packages(),
     install_package_data=True,
     install_requires=[
-        'pystyle',
-        'pynput',
+        'pyfade',
         'pymem',
-        'requests',
-        'json'
+        'requests'
     ]
 )
